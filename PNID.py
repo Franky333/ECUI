fe6acdea07a091592d5bae6be246e5b02913c497
markus <e11809597@student.tuwien.ac.at>
2019-08-09 19:22:01 +0200

    added pnid as background

--- PNID.py
+++ PNID.py
@@ -8,12 +8,24 @@ class PNID(QTabWidget):
 	def __init__(self, parent=None):
 		super(PNID, self).__init__(parent)
 
-		self.pnidLabel = QLabel()
-		self.pnidLabel.setPixmap(QPixmap("/Volumes/Data/markus/Programming/SpaceTeam/TXV_ECUI/pnid_v0.1.png"))
+		self.pnidWidget = QWidget()
+		self.pnidLabel = QLabel("", self)
 
-		self.
+		self.addTab(self.pnidLabel, "PNID")
+
+		#self.pnidLayout = QHBoxLayout()
+		#self.pnidLayout.addWidget(self.pnidLabel)
+
+		#self.setLayout(self.pnidLayout)
 
 
+
+	def loadPNID(self, path):
+		self.pnidPixmap = QPixmap(path)
+		w = self.pnidLabel.width()
+		h = self.pnidLabel.height()
+		print(w, h)
+		self.pnidLabel.setPixmap(self.pnidPixmap.scaled(w, h, Qt.KeepAspectRatio))
 
 if __name__ == '__main__':
 
@@ -21,12 +33,16 @@ if __name__ == '__main__':
 
 	app = QApplication(sys.argv)
 
+	pnid = PNID()
+
 	mainWindow = QMainWindow()
-	mainWindow.setCentralWidget(PNID())
+	mainWindow.setCentralWidget(pnid)
 	mainWindow.setMinimumSize(QSize(1920/2,1000))
 
 	mainWindow.setStyle(QStyleFactory.create("Macintosh"))
 
 	mainWindow.show()
 
+	pnid.loadPNID("/Volumes/Data/markus/Programming/SpaceTeam/TXV_ECUI/pnid_v0.1.png")
+	
 	sys.exit(app.exec_())
